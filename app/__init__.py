73ba4de013589ba19d40d1ce21b7b0774180f43c
CSopiko <skurd18@freeuni.edu.ge>
2021-12-03 15:25:55 +0400

    Add app initialization

---
+++ app/__init__.py
@@ -0,0 +1,18 @@
+SHIFTS_NUM = 3
+X_REPORT_NUM = 20
+Z_REPORT_NUM = 100
+
+
+def pos_simulation() -> None:
+    # customer shops
+    # cashier registers in receipt
+    # print receipt
+    # customer pay
+    # cashier close receipt
+    # ask manager for X report each 20th customer
+    # ask manager for Z report each 100th cutomer
+    pass
+
+
+if __name__ == '__main__':
+    pos_simulation()
